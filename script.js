1bc25b20a9841745f290833d0fe736b900f362a1
Yoserth Camacho <DanCaYos@outlook.com>
2024-02-25 15:51:04 -0400

    new js

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
 function encriptar() {
-    var texto= document.getElementById("inputTexto").value.toLowerCase();
-    var txtCifrado=texto.replace(/e/img, "enter");
-    var txtCifrado=txtCifrado.replace(/o/img, "ober");
-    var txtCifrado=txtCifrado.replace(/i/img, "imes");
-    var txtCifrado=txtCifrado.replace(/a/img, "ai");
-    var txtCifrado=txtCifrado.replace(/u/img, "ufat");
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+    var txtCifrado = texto.replace(/e/g, "enter")
+                          .replace(/o/g, "ober")
+                          .replace(/i/g, "imes")
+                          .replace(/a/g, "ai")
+                          .replace(/u/g, "ufat");
 
-    document.getElementById("imgDer").style.display = "none";
-    document.getElementById("texto").style.display = "none";
-    document.getElementById("texto2").innerHTML= txtCifrado;
-    document.getElementById("copiar").style.display = "show";
-    document.getElementById("copiar").style.display = "inherit";
+    actualizarInterfaz(txtCifrado);
 }
 
 function desencriptar() {
-    var texto= document.getElementById("inputTexto").value.toLowerCase();
-    var txtCifrado=texto.replace(/enter/img, "e");
-    var txtCifrado=txtCifrado.replace(/ober/img, "o");
-    var txtCifrado=txtCifrado.replace(/imes/img, "i");
-    var txtCifrado=txtCifrado.replace(/ai/img, "a");
-    var txtCifrado=txtCifrado.replace(/ufat/img, "u");
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+    var txtCifrado = texto.replace(/enter/g, "e")
+                          .replace(/ober/g, "o")
+                          .replace(/imes/g, "i")
+                          .replace(/ai/g, "a")
+                          .replace(/ufat/g, "u");
 
-    document.getElementById("imgDer").style.display = "none";
-    document.getElementById("texto").style.display = "none";
-    document.getElementById("texto2").innerHTML= txtCifrado;
-    document.getElementById("copiar").style.display = "show";
-    document.getElementById("copiar").style.display = "inherit";
+    actualizarInterfaz(txtCifrado);
 }
 
 function copiar() {
-    var contenido=document.querySelector("#texto2")
-    contenido.select()
-    document.execCommand("copy")
-   
+    var contenido = document.querySelector("#texto2");
+    contenido.select();
+    document.execCommand("copy");
+}
+
+function actualizarInterfaz(textoCifrado) {
+    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = textoCifrado;
+    document.getElementById("copiar").style.display = "block";
 }
